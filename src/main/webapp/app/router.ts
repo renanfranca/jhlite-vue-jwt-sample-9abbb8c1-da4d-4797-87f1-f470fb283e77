ba9ae7bb25f4afa31517d6e65c0a12802532da47
Renan Franca <renan.andradefranca@gmail.com>
2024-09-26 12:10:23 -0300

    refactor: add TODOs

--- src/main/webapp/app/router.ts
+++ src/main/webapp/app/router.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router';
 import { homeRoutes } from '@/home/application/HomeRouter';
+import { createRouter, createWebHistory } from 'vue-router';
 
+//TODO: add the auth routes
 export const routes = [...homeRoutes()];
 
 const router = createRouter({
