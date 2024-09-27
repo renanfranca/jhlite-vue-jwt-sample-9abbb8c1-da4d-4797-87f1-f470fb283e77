e2ec282fbde84d1fcac8d90c807de14610ad3959
Renan Franca <renan.andradefranca@gmail.com>
2024-09-27 16:56:54 -0300

    feat: add axios interceptors for authentication

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,4 +1,5 @@
 import { provideForAuth } from '@/auth/application/AuthProvider';
+import { setupAxiosInterceptors } from '@/shared/http/infrastructure/secondary/AxiosAuthInterceptor';
 import { AxiosHttp } from '@/shared/http/infrastructure/secondary/AxiosHttp';
 import axios from 'axios';
 import { createApp } from 'vue';
@@ -8,7 +9,9 @@ import router from './router';
 
 const app = createApp(AppVue);
 app.use(router);
-const axiosHttp = new AxiosHttp(axios.create({ baseURL: 'http://localhost:8080/' }));
+const axiosInstance = axios.create({ baseURL: 'http://localhost:8080/' });
+const axiosHttp = new AxiosHttp(axiosInstance);
 provideForAuth(axiosHttp);
+setupAxiosInterceptors(axiosInstance);
 // jhipster-needle-main-ts-provider
 app.mount('#app');
---
+++ src/main/webapp/app/shared/http/infrastructure/secondary/AxiosAuthInterceptor.ts
@@ -0,0 +1,26 @@
+import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import { inject } from '@/injections';
+import { AUTH_REPOSITORY } from '@/auth/application/AuthProvider';
+
+export const setupAxiosInterceptors = (axios: AxiosInstance): void => {
+  const auths = inject(AUTH_REPOSITORY);
+
+  axios.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
+    if (auths.isAuthenticated()) {
+      const token = auths.getToken();
+      config.headers.set('Authorization', `Bearer ${token}`);
+    }
+    return config;
+  });
+
+  axios.interceptors.response.use(
+    (response: AxiosResponse): AxiosResponse => response,
+    async (error: AxiosError): Promise<never> => {
+      if (error.response && error.response.status === 401) {
+        auths.logout();
+        //TODO: Redirect to login page or update application state
+      }
+      return Promise.reject(error);
+    }
+  );
+};
