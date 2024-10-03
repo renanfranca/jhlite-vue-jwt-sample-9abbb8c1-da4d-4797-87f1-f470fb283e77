18184b8480880c1a2d2ae996658a10154a20c743
Renan Franca <renan.andradefranca@gmail.com>
2024-10-03 09:22:38 -0300

    feat: add axios interceptor support to AxiosStub

--- src/test/webapp/unit/shared/http/infrastructure/secondary/AxiosStub.ts
+++ src/test/webapp/unit/shared/http/infrastructure/secondary/AxiosStub.ts
@@ -1,20 +1,53 @@
-import type { AxiosInstance, AxiosResponse } from 'axios';
-import sinon, { type SinonStub } from 'sinon';
+import type { AxiosInstance, AxiosInterceptorManager, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import type { SinonStub } from 'sinon';
+import sinon from 'sinon';
+
+export interface AxiosStubInterceptorManager extends AxiosInterceptorManager<InternalAxiosRequestConfig> {
+  use: SinonStub;
+  eject: SinonStub;
+  clear: SinonStub;
+}
 
 export interface AxiosStubInstance extends AxiosInstance {
   get: SinonStub;
   put: SinonStub;
   post: SinonStub;
   delete: SinonStub;
+  interceptors: {
+    request: AxiosStubInterceptorManager;
+    response: AxiosStubInterceptorManager;
+  };
+  runInterceptors: (config: InternalAxiosRequestConfig) => Promise<InternalAxiosRequestConfig>;
 }
 
-export const stubAxiosInstance = (): AxiosStubInstance =>
-  ({
+export const stubAxiosInstance = (): AxiosStubInstance => {
+  const instance = {
     get: sinon.stub(),
     put: sinon.stub(),
     post: sinon.stub(),
     delete: sinon.stub(),
-  }) as AxiosStubInstance;
+    interceptors: {
+      request: {
+        use: sinon.stub(),
+        eject: sinon.stub(),
+        clear: sinon.stub()
+      },
+      response: {
+        use: sinon.stub(),
+        eject: sinon.stub(),
+        clear: sinon.stub()
+      }
+    },
+    runInterceptors: async (config: InternalAxiosRequestConfig) => {
+      let currentConfig = { ...config, headers: config.headers || {} };
+      for (const interceptor of instance.interceptors.request.use.args) {
+        currentConfig = await interceptor[0](currentConfig);
+      }
+      return currentConfig;
+    }
+  } as AxiosStubInstance;
+  return instance;
+};
 
 export const dataAxiosResponse = <T>(data: T): AxiosResponse<T> =>
   ({
